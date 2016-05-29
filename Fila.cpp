c12acc73f8c365b1a1d02212d4a92355d92ce61f
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2016-05-29 08:30:34 -0300

    Inserindo.

---
+++ Fila.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int p, r;
+	cin >> p >> r;
+	if(p == 0) {
+		cout << "C";
+	} else {
+		if(r == 0) {
+			cout << "B";
+		} else {
+			cout << "A";
+		}
+	}
+	cout << endl;
+	
+	return 0;
+}
